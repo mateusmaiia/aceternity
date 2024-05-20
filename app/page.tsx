8863364a841a8875b8de325d197659c547a14788
maia <matteusmaia889@gmail.com>
2024-05-20 17:42:27 -0300

    deleted

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,11 @@
 import { Grid } from '@/components/Grid'
 import { Hero } from '@/components/Hero'
 import { FloatingNav } from '@/components/ui/FloatingNav'
-import { FaHome } from 'react-icons/fa'
+import { navItems } from '@/data'
 
 export default function Home() {
 
-  const navItems = [
-    {
-      name: 'Home',
-      link: '/',
-      icon: <FaHome />
-    },
-    
-  ]
+
 
   return (
     <main className='relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5'>
